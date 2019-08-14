c709518a1ffc71df53b607578f6e7821bd96adeb
Wilson Guiraldelli <wilson.guiraldelli@mazzafc.tech>
2019-08-13 22:20:21 -0300

    Primeira vez que vai

---
+++ src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.unifacef.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.unifacef.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MeuPrimeiroProjetoSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
