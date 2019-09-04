cb4ab6007c1644ef0471cdb5a73c869acedd411c
Wilson Guiraldelli <wilson.guiraldelli@mazzafc.tech>
2019-09-03 22:19:07 -0300

    Aula 03/09/2019

--- src/main/java/br/com/unifacef/api/entities/Empresa.java
+++ src/main/java/br/com/unifacef/api/entities/Empresa.java
@@ -1,5 +1,115 @@
 package br.com.unifacef.api.entities;
 
-public class Empresa {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa implements Serializable {
+	private static final long serialVersionUID = -367604013937942025L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "razao_social", nullable = false)
+	private String razaoSocial;
+
+	@Column(name = "cnpj", nullable = false)
+	private String cnpj;
+
+	@Column(name = "data_criacao", nullable = false)
+	private Date dataCriacao;
+
+	@Column(name = "data_atualizacao", nullable = false)
+	private Date dataAtualizacao;
+
+	// Objeto empresa tem relação com funcionario (1:N). É uma relação mapeada por
+	// 'empresa'.
+	// Fetch - lazy(preguiçoso) não retorna os funcionarios da empresa
+	// Cascade - Quando matar uma empresa, mata todos os funcionarios
+	@OneToMany(mappedBy = "empresa", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Funcionario> funcionarios;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		dataAtualizacao = new Date();
+	}
+
+	@PrePersist
+	public void prePersist() {
+		final Date atual = new Date();
+		dataCriacao = atual;
+		dataAtualizacao = atual;
+	}
+
+	@Override
+	public String toString() {
+		return "Empresa [id=" + id + ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + ", dataCriacao=" + dataCriacao
+				+ ", dataAtualizacao=" + dataAtualizacao + ", funcionarios=" + funcionarios + "]";
+	}
+	
+	
 
 }
